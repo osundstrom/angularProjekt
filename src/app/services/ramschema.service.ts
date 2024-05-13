c2aafcbf7d1c2f2cceb1bab134aa66b290bd86d6
Oskar Sundström <ossu2300@student.miun.se>
2024-05-13 23:16:23 +0200

    updated styles, update service

--- src/app/services/ramschema.service.ts
+++ src/app/services/ramschema.service.ts
@@ -11,4 +11,25 @@ export class RamschemaService {
 
   constructor() { }
  
+  getRamschema1(): Courses[] {
+    console.log("ram1");
+    const  data = localStorage.getItem("addedOne");
+    console.log(data)
+    return data ? JSON.parse(data): [];
+  }
+
+  getRamschema2(): Courses[] {
+    console.log("ram2");
+    const data = localStorage.getItem("addedTwo");
+    console.log(data)
+    return data ? JSON.parse(data) : [];
+  }
+
+  getRamschema3(): Courses[] {
+    console.log("ram3");
+    const data = localStorage.getItem("addedThree");
+    console.log(data)
+    return data ? JSON.parse(data): [];
+  }
+
 }
