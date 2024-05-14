35e3335522c7b1f1b2066b3491258525359bbfe4
Oskar Sundström <ossu2300@student.miun.se>
2024-05-15 00:48:31 +0200

    updated error page, lagt till bilder via typescript idtället.

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -9,5 +9,5 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './error.component.scss'
 })
 export class ErrorComponent {
-
+  errorImage: string = "/assets/images/errorPhone.jpg";
 }
